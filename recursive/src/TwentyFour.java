c2d25c9b07fb9e2a71f70e1bca2e7df256526c08
Dongli99 <dongliliu0@gmail.com>
2024-04-17 10:07:58 -0700

    working version to refine.

--- recursive/src/TwentyFour.java
+++ recursive/src/TwentyFour.java
@@ -1,49 +1,142 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class TwentyFour {
-	private final double TARGET = 24;
-	public static String play(double a, double b, double c, double d) {
+
+	private static class Operation {
+		private BinaryOperator<Double> o;
+		private char s;
+
+		public Operation(char operator) {
+			switch (operator) {
+				case '+':
+					this.o = (a, b) -> a + b;
+					break;
+				case '-':
+					this.o = (a, b) -> a - b;
+					break;
+				case '*':
+					this.o = (a, b) -> a * b;
+					break;
+				case '/':
+					this.o = (a, b) -> a / b;
+					break;
+				default:
+					throw new IllegalArgumentException("Invalid operator: " + operator);
+			}
+			this.s = operator;
+		}
+
+		public Double calc(Double a, Double b) {
+			return this.o.apply(a, b);
+		}
+
+		@Override
+		public String toString() {
+			return String.valueOf(this.s);
+		}
+	}
+
+	private static class OpUnit {
+		private Double a = null;
+		private Double b = null;
+		private Operation o = null;
+		private Double r;
+
+		public OpUnit(Double a) {
+			this.a = a;
+			r = a;
+		}
+
+		public OpUnit(Double a, Double b, Operation o) {
+			this.a = a;
+			this.b = b;
+			this.o = o;
+			r = o.calc(this.a, this.b);
+		}
+
+		public OpUnit(OpUnit a, OpUnit b, Operation o) {
+			this(a.r, b.r, o);
+		}
+
+		public Double r() {
+			return r;
+		}
+
+		@Override
+		public String toString() {
+			return a + o.toString() + b;
+		}
+	}
+
+	private static final Integer TARGET = 24;
+	private static final Operation p = new Operation('+');
+	private static final Operation m = new Operation('-');
+	private static final Operation t = new Operation('*');
+	private static final Operation d = new Operation('/');
+	private static final Operation[] os = { p, m, t, d };
+
+	public static void play(int a, int b, int c, int d) {
+		String solution = makeNum(
+				Double.valueOf(TARGET),
+				new OpUnit(Double.valueOf(a)),
+				new OpUnit(Double.valueOf(b)),
+				new OpUnit(Double.valueOf(c)),
+				new OpUnit(Double.valueOf(d)));
+		solution = solution != null ? solution : "No answer.";
+		System.out.println(solution);
+	}
+
+	private static String makeNum(Double target, OpUnit... us) {
+		if (us.length == 0)
+			return null;
+		List<List<OpUnit>> orders = new ArrayList<>();
+		List<OpUnit> origin = new ArrayList<>();
+		for (OpUnit u : us) {
+			origin.add(u);
+		}
+		genOrders(origin, 0, orders);
+		for (Operation o : os) {
+			for (List<OpUnit> od : orders) {
+				if (us.length == 2) {
+					OpUnit u = new OpUnit(od.get(0), od.get(1), o);
+					if (u.r().equals(target))
+						return u.toString();
+				} else {
+					OpUnit u = new OpUnit(od.get(0), od.get(1), o);
+					OpUnit[] remain = new OpUnit[us.length - 1];
+					remain[0] = u;
+					System.arraycopy(us, 2, remain, 1, us.length - 2);
+					String result = makeNum(target, remain);
+					if (result != null) {
+						return result;
+					}
+				}
+			}
+		}
 		return null;
 	}
-	
-	public static String makeNum(double a, double b, double c, double target) {
-		if (makeNum(b, c, target - a) != null) {
-			return makeNum(b, c, target - a);
+
+	private static void genOrders(List<OpUnit> origin, int index, List<List<OpUnit>> orders) {
+		if (index == origin.size()) {
+			orders.add(new ArrayList<>(origin));
+		} else {
+			for (int i = index; i < origin.size(); i++) {
+				swap(origin, index, i);
+				genOrders(origin, index + 1, orders);
+				swap(origin, index, i); // Backtrack
+			}
 		}
-		if (makeNum(a, c, target - b) != null) {
-			return makeNum(a, c, target - b);
-		}
-		if (makeNum(a, b, target - c) != null) {
-			return makeNum(a, b, target - c);
-		}
-		return null;
 	}
-	
-	public static String makeNum(double a, double b, double target) {
-		if (a < b) {
-			double tmp = a;
-			a = b;
-			b = tmp;
-		}
-		if (a + b == target)
-			return a + " + " + b;
-		if (a * b == target)
-			return  a + " * " + b;
-		if (a / b == target)
-			return "a / b";
-		if (a - b == target)
-			return "a - b";
-		return null;
+
+	private static void swap(List<OpUnit> origin, int i, int j) {
+		OpUnit temp = origin.get(i);
+		origin.set(i, origin.get(j));
+		origin.set(j, temp);
 	}
-	
-	public static ArrayList<Double> createCombs(double a, double b){
-		ArrayList<Double> res = new ArrayList<Double>();
- 		res.addAll( Arrays.asList(a + b, a * b, a / b, b / a, a - b, b - a));
- 		return res;
-	}
+
 	public static void main(String[] args) {
-		String res = makeNum(2, 12, 6, 24);
-		System.out.print(res);
+		play(6, 6, 6, 6);
 	}
 }
